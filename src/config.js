378c92e63e8bf666763dce8ebe2ac7a47ebbff54
Anoop Kumar <anoop_kr@yahoo.com>
2019-11-01 17:40:29 +0530

    Initial client checkin : Logs into Project stack

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "redbrickai-projects-dev-attachmentsbucket-lzntba8iglc9"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ojoixds597.execute-api.us-east-1.amazonaws.com/dev/projects"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_uoxM9S7xq",
+    APP_CLIENT_ID: "6meq710dkm30a8opr0dajuvp3v",
+    IDENTITY_POOL_ID: "us-east-1:8383f30d-83f0-40ab-b76d-3025650d34a5"
+  }
+};
